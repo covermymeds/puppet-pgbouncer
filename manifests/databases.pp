1c27dde15760c99cfe361143410a4e1de8f686c9
CJ Estel <cestel@covermymeds.com>
2015-08-25 16:23:42 -0400

    added check to make sure variable was set

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -4,9 +4,11 @@ define pgbouncer::databases(
 
   validate_array($databases)
 
-  concat::fragment { "${databases[0]['dest_db']}_${databases[0]['auth_user']}":
-    target  => '/etc/pgbouncer/pgbouncer.ini',
-    content => template('pgbouncer/pgbouncer.ini.databases.part2.erb'),
-    order   => '02',
+  unless empty($databases[0]) {
+    concat::fragment { "${databases[0]['dest_db']}_${databases[0]['auth_user']}":
+      target  => '/etc/pgbouncer/pgbouncer.ini',
+      content => template('pgbouncer/pgbouncer.ini.databases.part2.erb'),
+      order   => '02',
+    }
   }
 }
