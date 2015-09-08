f9ccc2abc6e76f5dc29f4c5268e4374f83abe718
CJ Estel <cestel@covermymeds.com>
2015-09-08 14:13:37 -0400

    changed line spacing for 80 char limit

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -10,8 +10,8 @@ define pgbouncer::databases(
   validate_array($databases)
 
   unless empty($databases[0]) {
-    concat::fragment {
-    "${databases[0]['dest_db']}_${databases[0]['auth_user']}":
+    $uniq_name = "${databases[0]['dest_db']}_${databases[0]['auth_user']}"
+    concat::fragment { $uniq_name:
       target  => $::pgbouncer::conffile,
       content => template('pgbouncer/pgbouncer.ini.databases.part2.erb'),
       order   => '02',
